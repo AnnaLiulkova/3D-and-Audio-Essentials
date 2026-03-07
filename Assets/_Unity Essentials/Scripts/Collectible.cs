8694dcd7eb21d290f742bce1dafe3e5e6e61b2e8
AnnaLiulkova <al644722@gmail.com>
2026-03-07 21:25:34 +0200

    03: Implement collectible pickup logic
    - Implemented OnTriggerEnter to detect player collision.
    - Configured the collectible object to disappear upon contact.

--- Assets/_Unity Essentials/Scripts/Collectible.cs
+++ Assets/_Unity Essentials/Scripts/Collectible.cs
@@ -4,6 +4,7 @@ public class Collectible : MonoBehaviour
 {
 
     public float rotationSpeed;
+    public GameObject onCollectEffect;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,4 +17,17 @@ public class Collectible : MonoBehaviour
     {
         transform.Rotate(0, rotationSpeed, 0);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Destroy the collectible
+            Destroy(gameObject);
+
+            // instantiate the particle effect
+            Instantiate(onCollectEffect, transform.position, transform.rotation);
+        }
+    }
+
 }
